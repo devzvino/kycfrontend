ddb11005d4bab445fc5c5173f97d8a38f9b2864e
l3ftie <nkarodza@gmail.com>
2022-07-14 13:50:57 +0200

    quickUpdate

--- components/Regconfirm.js
+++ components/Regconfirm.js
@@ -1,29 +1,51 @@
-import React from 'react'
-import{View,Image, Text, Dimensions} from 'react-native';
-import SvgComponent from './SuccessSvg'
-import SignUpNavigationButton from './SignUpNavigationButton';
+import React from "react";
+import { View, Image, Text, Dimensions } from "react-native";
+import SvgComponent from "./SuccessSvg";
+import SignUpNavigationButton from "./SignUpNavigationButton";
 
 //App theme styles
-import { FontTheme,ButtonTheme,ImageBackgroundTheme,LogoTheme,InputTheme } from '../components/ThemeFile';
+import {
+  FontTheme,
+  ButtonTheme,
+  ImageBackgroundTheme,
+  LogoTheme,
+  InputTheme,
+} from "../components/ThemeFile";
 
 //Device Dimensions
-const {width, height} = Dimensions.get('screen');
+const { width, height } = Dimensions.get("screen");
 
 function RegConfirm() {
   return (
-   <View style={{width:width/1.15, alignSelf:'center', alignItems:'center' ,flex:1}}>
-    <SvgComponent/>
-    <Text style={FontTheme.footText}>
-    You have successfully verified your national identity</Text> 
-    <Text style={FontTheme.footText}>
-    Please Proceed to add your addresses to start the address verification prosess 
-    </Text>
-    <View style= {{width:width, height:'15%', alignItems: "center", position:'absolute', bottom:0,}}>
-      <SignUpNavigationButton title={'Continue'} />
-     </View>
-   </View>
-   
-  )
+    <View
+      style={{
+        width: width / 1.15,
+        alignSelf: "center",
+        alignItems: "center",
+        flex: 1,
+      }}
+    >
+      <SvgComponent />
+      <Text style={FontTheme.footText}>
+        You have successfully verified your national identity
+      </Text>
+      <Text style={FontTheme.footText}>
+        Please Proceed to add your addresses to start the address verification
+        prosess
+      </Text>
+      <View
+        style={{
+          width: width,
+          height: "15%",
+          alignItems: "center",
+          position: "absolute",
+          bottom: 0,
+        }}
+      >
+        <SignUpNavigationButton title={"Continue"} />
+      </View>
+    </View>
+  );
 }
 
 export default RegConfirm;
--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,51 +1,54 @@
-import React from 'react';
-import { Text, View,TextInput, Image, Keyboard} from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { useEffect, useState } from 'react';
+import React from "react";
+import { Text, View, TextInput, Image, Keyboard } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useEffect, useState } from "react";
 
-import mainLogo from '../assets/images/kyc-logo.png';
+import mainLogo from "../assets/images/kyc-logo.png";
 
-
-import { Dimensions,ScrollView } from "react-native";
-import IdUpload from '../components/IdUpload';
-import UserDetails from '../components/UserDetails';
-import OTPConfirm from '../components/OTPConfirm';
-import SignUpNavigationButton from '../components/SignUpNavigationButton';
-import RegConfirm from '../components/RegConfirm';
-
-import { LogoTheme } from '../components/ThemeFile';
+import { Dimensions, ScrollView } from "react-native";
+import IdUpload from "../components/IdUpload";
+import UserDetails from "../components/UserDetails";
+import OTPConfirm from "../components/OTPConfirm";
+import { LogoTheme } from "../components/ThemeFile";
+import RegConfirm from "../components/RegConfirm";
 
 //Device Dimenstions
-const {width, height} = Dimensions.get('screen');
-
+const { width, height } = Dimensions.get("screen");
 
 const SignUp = () => {
   // state randering
 
-  const [userView, setuserView] = useState(true)
-  const [idUploadView, setidUploadView] = useState(false)
-  const [otpConfrimView, setOtpConfrimView] = useState(false)
-  const [regConfrimView, setRegConfrimView] = useState(false)
-
+  const [userView, setuserView] = useState(true);
+  const [idUploadView, setidUploadView] = useState(false);
+  const [otpConfrimView, setOtpConfrimView] = useState(false);
+  const [regConfrimView, setRegConfrimView] = useState(false);
 
   return (
-    <ScrollView  contentContainerStyle={{width:width, height:height  }}>
-        <View style={{width:'100%', height:'20%', marginLeft:'8%', paddingTop: '10%',}}>
-      <Image source={mainLogo} style={LogoTheme.miniLogo}/>
+    <ScrollView contentContainerStyle={{ width: width, height: height }}>
+      <View
+        style={{
+          width: "100%",
+          height: "20%",
+          marginLeft: "8%",
+          paddingTop: "10%",
+        }}
+      >
+        <Image source={mainLogo} style={LogoTheme.miniLogo} />
       </View>
       {/* Render Form elements here */}
 
       {userView && <UserDetails />}
       {idUploadView && <IdUpload />}
-      {otpConfrimView &&  <OTPConfirm />}
-      {regConfrimView &&  <RegConfirm />}
-    
-      </ScrollView>
-  )
+      {otpConfrimView && <OTPConfirm />}
+      {regConfrimView && <RegConfirm />}
+    </ScrollView>
+  );
 };
 
 export default SignUp;
 
-   {/* 
+{
+  /* 
       <OTPConfirm setOtpConfirm={setOtpConfirm}/>
-   */}
+   */
+}
