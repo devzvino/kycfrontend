722ae54603338c1cc5a8ad5047b7a4f4db22cdf7
L3ftie <nkarodza@gmail.com>
2023-02-09 14:53:44 +0200

    updated env vars

--- environmentVariables.js
+++ environmentVariables.js
@@ -4,7 +4,7 @@ export const keys = {
   apiSecret: "sUG1kJh9EIBRpgCF",
   kycUser: "celestial",
   kycPassword: "qwertyui",
-  // apiURL: "https://kycbackendapp.herokuapp.com/", //  production ipaddress
-  apiURL: "http://192.168.1.177:4000/", //  home nyasha ipaddress
+  apiURL: "https://kycbackendapp.herokuapp.com/", // production ipaddress
+  // apiURL: "http://192.168.1.177:4000/", // home nyasha ipaddress
   // apiURL: "http://10.70.14.236:4000/", //  work ipaddress
 };
