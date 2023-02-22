13306b8e7f8635a92f497eb4dfbf7c73e0ba9d22
devzvino <developerzvinokwazvo@gmail.com>
2023-02-22 16:07:21 +0200

    place holders

---
+++ screens/Onboarding1.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Onboarding1 = () => {
+    return (
+        <View>
+            <Text>Onboarding1</Text>
+        </View>
+    )
+}
+
+export default Onboarding1
---
+++ screens/Onboarding2.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Onboarding2 = () => {
+    return (
+        <View>
+            <Text>Onboarding2</Text>
+        </View>
+    )
+}
+
+export default Onboarding2
