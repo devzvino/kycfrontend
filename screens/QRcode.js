7dcc1da37d4f5e24d3a8b9dc60648d73e385fd5b
L3ftie <nkarodza@gmail.com>
2023-02-23 11:33:51 +0200

    pdf update

--- screens/QRcode.js
+++ screens/QRcode.js
@@ -24,10 +24,106 @@ const QRcode = () => {
 
   const [PdfData, setPdfData] = useState("");
 
-  const element = <QRCode size={270} codeStyle="circle" logo={kycLogo} logoSize={40} content={"name"} />;
+  // const element = <QRCode size={270} codeStyle="circle" logo={kycLogo} logoSize={40} content={"name"} />;
 
-  console.log(PdfData);
-  let html;
+  // console.log(PdfData);
+  console.log(PdfData[0]);
+
+  //   let html = `
+  //   <html style="margin:0 !important">
+  //   <head>
+  //   <style>
+  //   p,h4,h1,h2,h3,h5,h6 {margin:0;}
+  // </style>
+  //    </head>
+  //     <body style="margin:0">
+  //      <div>
+  //          <p style="color:green;">Personal details</p>
+  //          <div style="display:flex; justify-content: space-between; width:100%; ">
+  //            <div style="flex: 1;">
+  //              <div style="display:flex; gap:10px;">
+  //                <div style="margin:0">
+  //                  <p>Name(S):</p>
+  //                  <h4>${PdfData[0].details.firstname}</h4>
+  //                </div>
+  //                <div style="margin:0">
+  //                  <p>Surname:</p>
+  //                  <h4>${PdfData[0].details.surname}</h4>
+  //                </div>
+  //              </div>
+  //              <div style="display:flex; gap:10px;">
+  //                <div style="margin:0">
+  //                  <p>National ID No:</p>
+  //                  <h4>${PdfData[0].details.idNumber}</h4>
+  //                </div>
+  //                <div style="margin:0">
+  //                  <p>Phone Number:</p>
+  //                  <h4>${PdfData[0].details.phone}</h4>
+  //                </div>
+  //              </div>
+  //              <p>Registered on: ${moment(PdfData[0].details.createdAt).format("lll")}</p>
+  //              <p style="font-size: 0.875rem;">
+  //                This is the date when ${PdfData[0]?.details.firstname} ${PdfData[0]?.details.surname}
+  //                registered on the KYC Africa National Identity & Address Verification Platform / System.
+  //              </p>
+  //            </div>
+  //            <div style="flex: 1;">
+  //            ${React.renderToString(
+  //              <QRCode size={270} codeStyle="circle" logo={kycLogo} logoSize={40} content={"name"} />
+  //            )}
+  //            </div>
+  //          </div>
+  //      </div>
+
+  //      <div style="border-top: 1px dotted green; padding-top:10px; margin-top:10px">
+  //        <p style="color:green;">${PdfData[0].details.firstname}'s Home Locations</p>
+  //        <div style="background-color: #f6f6f6;  border-radius: 5px; padding:10px;  box-shadow:inset 5px 0px 0px 0px green;>
+  //        ${
+  //          PdfData[0].home.length > 0
+  //            ? PdfData[0].home
+  //                .map(
+  //                  (home) => `<div>
+  //          <p style="font-size: 0.875rem;">Address : ${home.streetName}</p>
+  //          <p style="font-size: 0.875rem;">Surburb : ${home.suburb}</p>
+  //          <p style="font-size: 0.875rem;">City : ${home.city}</p>
+  //          <p style="font-size: 0.875rem;">Added On : ${moment(home.createdAt).format("lll")}</p>
+  //          <p style="font-size: 0.875rem;">Verification State : ${home.homeVerified}</p>
+  //          <p style="font-size: 0.875rem;">Positive Verification Checks : ${home.homeTotalCount}</p>
+  //          <p style="font-size: 0.875rem;">Total Verification Checks: : ${home.homeVerificationCount}</p>
+  //        </div>`
+  //                )
+  //                .join("")
+  //            : `<p>${PdfData[0].details.firstname} ${PdfData[0].details.surname} has not set any home locations.</p>`
+  //        }
+  //        </div>
+  //      </div>
+
+  //      <div style="border-top: 1px dotted green; padding-top:10px; margin-top:10px">
+  //        <p style="color:green;">${PdfData[0].details.firstname}'s Work Locations</p>
+  //        <div>
+  //        ${
+  //          PdfData[0].work.length > 0
+  //            ? PdfData[0]?.work
+  //                .map(
+  //                  (work) => `<div>
+  //          <p>Address : ${work.streetName}</p>
+  //          <p>Surburb : ${work.suburb}</p>
+  //          <p>City : ${work.city}</p>
+  //          <p>Added On : ${moment(work.createdAt).format("lll")}</p>
+  //          <p>Verification State : ${work.workVerified}</p>
+  //          <p>Positive Verification Checks : ${work.workTotalCount}</p>
+  //          <p>Total Verification Checks: : ${work.workVerificationCount}</p>
+  //        </div>`
+  //                )
+  //                .join("")
+  //            : `<p>${PdfData[0].details.firstname} ${PdfData[0].details.surname} has not set any work locations.</p>`
+  //        }
+  //        </div>
+  //      </div>
+  //    </body>
+  //   </html>
+  // `;
+
   const gatheringAllUserLocations = () => {
     setLoading(true);
     let fetchOk = (...args) =>
@@ -57,111 +153,16 @@ const QRcode = () => {
           },
         ]);
 
-        //++++++++++++++++++++++++++++++++++++
-
-        html = `
-     <html style="margin:0 !important">
-     <head>
-     <style>
-     p,h4,h1,h2,h3,h5,h6 {margin:0;}
-   </style>
-      </head>
-       <body style="margin:0">
-        <div>
-            <p style="color:green;">Personal details</p>
-            <div style="display:flex; justify-content: space-between; width:100%; ">
-              <div style="flex: 1;">
-                <div style="display:flex; gap:10px;">
-                  <div style="margin:0">
-                    <p>Name(S):</p>
-                    <h4>${PdfData[0].details.firstname}</h4>
-                  </div>
-                  <div style="margin:0">
-                    <p>Surname:</p>
-                    <h4>${PdfData[0].details.surname}</h4>
-                  </div>
-                </div>
-                <div style="display:flex; gap:10px;">
-                  <div style="margin:0">
-                    <p>National ID No:</p>
-                    <h4>${PdfData[0].details.idNumber}</h4>
-                  </div>
-                  <div style="margin:0">
-                    <p>Phone Number:</p>
-                    <h4>${PdfData[0].details.phone}</h4>
-                  </div>
-                </div>
-                <p>Registered on: ${moment(PdfData[0].details.createdAt).format("lll")}</p>
-                <p style="font-size: 0.875rem;">
-                  This is the date when ${PdfData[0]?.details.firstname} ${PdfData[0]?.details.surname} 
-                  registered on the KYC Africa National Identity & Address Verification Platform / System.
-                </p>
-              </div>
-              <div style="flex: 1;">
-                ${element}
-              </div>
-            </div>
-        </div>
-
-        <div style="border-top: 1px dotted green; padding-top:10px; margin-top:10px">
-          <p style="color:green;">${PdfData[0].details.firstname}'s Home Locations</p>
-          <div style="background-color: #f6f6f6;  border-radius: 5px; padding:10px;  box-shadow:inset 5px 0px 0px 0px green;>
-          ${
-            PdfData[0].home.length > 0
-              ? PdfData[0].home
-                  .map(
-                    (home) => `<div> 
-            <p style="font-size: 0.875rem;">Address : ${home.streetName}</p> 
-            <p style="font-size: 0.875rem;">Surburb : ${home.suburb}</p> 
-            <p style="font-size: 0.875rem;">City : ${home.city}</p> 
-            <p style="font-size: 0.875rem;">Added On : ${moment(home.createdAt).format("lll")}</p> 
-            <p style="font-size: 0.875rem;">Verification State : ${home.homeVerified}</p> 
-            <p style="font-size: 0.875rem;">Positive Verification Checks : ${home.homeTotalCount}</p> 
-            <p style="font-size: 0.875rem;">Total Verification Checks: : ${home.homeVerificationCount}</p> 
-          </div>`
-                  )
-                  .join("")
-              : `<p>${PdfData[0].details.firstname} ${PdfData[0].details.surname} has not set any home locations.</p>`
-          } 
-          </div>
-        </div>
-
-        <div style="border-top: 1px dotted green; padding-top:10px; margin-top:10px">
-          <p style="color:green;">${PdfData[0].details.firstname}'s Work Locations</p>
-          <div>
-          ${
-            PdfData[0].work.length > 0
-              ? PdfData[0]?.work
-                  .map(
-                    (work) => `<div> 
-            <p>Address : ${work.streetName}</p> 
-            <p>Surburb : ${work.suburb}</p> 
-            <p>City : ${work.city}</p> 
-            <p>Added On : ${moment(work.createdAt).format("lll")}</p> 
-            <p>Verification State : ${work.workVerified}</p> 
-            <p>Positive Verification Checks : ${work.workTotalCount}</p> 
-            <p>Total Verification Checks: : ${work.workVerificationCount}</p> 
-          </div>`
-                  )
-                  .join("")
-              : `<p>${PdfData[0].details.firstname} ${PdfData[0].details.surname} has not set any work locations.</p>`
-          } 
-          </div>
-        </div>
-      </body>
-     </html>
-  `;
-
-        //++++++++++++++++++++++++++++++++++++
-
         setLoading(false);
       })
       .catch((e) => console.error(e));
+    generateKycPdf();
   };
 
   const generateKycPdf = async () => {
     // generating
-    gatheringAllUserLocations();
+    await gatheringAllUserLocations();
+
     const file = await printToFileAsync({
       html: html,
       base64: false,
@@ -203,7 +204,7 @@ const QRcode = () => {
       <View style={{ alignItems: "center", marginTop: 20 }}>
         <TouchableOpacity
           disabled={loading}
-          onPress={generateKycPdf}
+          onPress={() => generateKycPdf()}
           style={{
             borderColor: ColorTheme.grey2,
             borderRightWidth: 3,
